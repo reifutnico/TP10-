d1c05165add5066da3a8068a3de85803e7a3e35c
Tobias Zaselsky <95321726+tobizasel@users.noreply.github.com>
2023-11-02 13:25:29 -0300

    Add files via upload

---
+++ TP10--main/Models/Series.cs
@@ -0,0 +1,9 @@
+ public class Series
+{
+    public int IdSerie {get;set;}
+    public string Nombre {get;set;}
+    public int AñoIinicio {get;set;}
+    public string Sinopsis {get;set;}
+    public string ImagenSerie {get;set;}
+
+}
---
+++ TP10--main/wwwroot/js/site.js
@@ -0,0 +1,60 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const mostrarModalSerie = (idBoton) => {
+mostrarModalActores
+    $.ajax(
+        {
+            type: "POST",
+            datatype: "JSON",
+            url: "/Home/ModalSeries",
+            data: {Id: idBoton},
+            success: (response) => {
+                console.log(response);
+                $("#contenido__img").attr('src',response.imagenSerie),
+                $("#contenido__parrafo").html(response.nombre)
+            }
+        }
+    )
+}
+
+const mostrarModalActores = (idBoton) => {
+
+
+    $.ajax(
+        {
+            type: "POST",
+            datatype: "JSON",
+            url: "/Home/ModalActores",
+            data: {Id: idBoton},
+            success: (response) => {
+                
+                console.log(response);
+                $("#contenido__img").attr('src', "#"),
+                response.forEach(a => {
+                    $("#contenido__parrafo").append(a.nombre)
+                });
+
+            
+        }
+        }
+    )
+}
+
+const mostrarModalTemporada = (idBoton) => {
+
+    $.ajax(
+        {
+            type: "POST",
+            datatype: "JSON",
+            url: "/HomeController/ModalTemporada",
+            data: {Id: idBoton},
+            success: (response) => {
+                $("#contenido__parrafo").html(response.tituloTemporada)
+                $("#contenido__img").attr('src', "#")
+            }
+        }
+    )
+}
